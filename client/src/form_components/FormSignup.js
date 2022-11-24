837c261a3d4167832db2a595fa95f6d2ad267dc7
Obycoder8 <90784287+Obycoder8@users.noreply.github.com>
2022-11-24 03:24:41 +0300

    Update FormSignup.js

--- client/src/form_components/FormSignup.js
+++ client/src/form_components/FormSignup.js
@@ -13,7 +13,7 @@ const FormSignup = () => {
 
   const submitReview = async (e) => {
     e.preventDefault();
-    Axios.post("https://client-production-84ce.up.railway.app/api/insert", {
+    Axios.post("https://client-production-84ce.up.railway.app:3001/api/insert", {
       userName: userName,
       password: password,
       email: email,
